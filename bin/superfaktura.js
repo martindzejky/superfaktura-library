5993267cf019cb4496fd9b26c14f0b1647c4fc04
Martin Jakubik <martindzejky@gmail.com>
2026-02-16 22:13:07 +0100

    move dotenv to cli only

--- bin/superfaktura.js
+++ bin/superfaktura.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
+import { config as loadDotEnv } from 'dotenv';
 import { runCli } from '../dist/cli.js';
+
+loadDotEnv({ quiet: true });
 
 runCli(process.argv).catch((error) => {
   const message = error instanceof Error ? error.message : 'Unknown error occurred.';
--- src/client/create-client.ts
+++ src/client/create-client.ts
@@ -1,4 +1,3 @@
-import { config as loadDotEnv } from 'dotenv';
 import { createAuthHeader } from '../core/auth-header';
 import { DEFAULT_BASE_URL } from '../core/constants';
 import { HttpClient } from '../core/http-client';
@@ -10,8 +9,6 @@ import { parseCompanyId } from '../utils';
 const DEFAULT_TIMEOUT_MS = 15_000;
 
 export function createClient(config: ClientConfig = {}) {
-  loadDotEnv({ quiet: true });
-
   const apiEmail = config.apiEmail ?? process.env.SUPERFAKTURA_API_EMAIL;
   const apiKey = config.apiKey ?? process.env.SUPERFAKTURA_API_KEY;
   const baseUrl = config.baseUrl ?? process.env.SUPERFAKTURA_API_URL ?? DEFAULT_BASE_URL;
